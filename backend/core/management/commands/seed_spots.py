018e2220ca89f90b7dd60555fe681896dc842466
aaacmsg <aaacmsg@gmail.com>
2025-09-09 16:51:46 -0700

    Delete backend/core directory

--- backend/core/management/commands/seed_spots.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from core.models import ParkingSpot
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        for i in range(1, 26):
-            ParkingSpot.objects.get_or_create(number=n, defaults={'is_occupied': False})
-        self.stdout.write(self.style.SUCCESS('Successfully seeded 25 parking spots.'))
